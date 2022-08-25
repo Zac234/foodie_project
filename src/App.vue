ff724c257e9bcffd436857b7d2154279376ef4ad
Zach <zacbenincasa@gamil.com>
2022-08-25 16:59:24 -0600

    fixed error on pages

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import clientHeadVue from './components/clientHead.vue';
 
+import clientMainVue from './components/clientMain.vue';
+import clientFootVue from './components/clientFoot.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    clientHeadVue,
+    clientMainVue,
+    clientFootVue
   }
 }
 </script>
